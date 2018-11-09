b8ad5d909c7f7eb90342faf389c5ff49ec652deb
Gustavo Ercoli <ruben.ercoli@gmail.com>
2018-11-09 16:12:15 -0300

    Corrected path AppNavigator

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import AppReducer from './app/reducers';
-import { AppNavigator, middleware } from './app/components/appNavigator';
+import { AppNavigator, middleware } from './app/components/AppNavigator';
 
 
 const store = createStore(AppReducer, applyMiddleware(thunk));
